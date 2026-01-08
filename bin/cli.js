1b99fe89b974a020a0a19ff3a1618cfc38445023
Igor <igorcostalins2005@gmail.com>
2026-01-08 00:34:57 -0300

    feat(cli): create executable entry point with env configuration

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import 'dotenv/config';
+import { init, run } from "../src/commands/index.js";
+import logger from "../src/utils/logger.js";
+
+const args = process.argv.slice(2);
+const command = args[0];
+
+switch (command) {
+    case 'init':
+        await init();
+        break;
+    case 'run':
+        const subCommand = args[1];
+        await run(subCommand);
+        break;
+    default:
+        logger.warn('Uso: gen-commit init | run');
+}
